eacaf7d9d72a09315540f775ecc0169560888e85
Juan Esteban Gonzalez <e-gonzalez@outlook.com>
2020-08-17 14:02:15 -0400

    Agregar archivos de proyecto.

---
+++ Practica 4-1 MVC-ActionLink-DiplomadoUASD/Controllers/ProveedoresController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Practica_4_1_MVC_ActionLink_DiplomadoUASD.Controllers
+{
+    public class ProveedoresController : Controller
+    {
+        // GET: Proveedores
+        public ActionResult Listado()
+        {
+            return View();
+        }
+        public ActionResult Crear()
+        {
+            return View();
+        }
+    }
+}
---
+++ Practica 4-1 MVC-ActionLink-DiplomadoUASD/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Practica_4_1_MVC_ActionLink_DiplomadoUASD.MvcApplication" Language="C#" %>
